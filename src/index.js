b239f448837449230686271814f26fbd2f96c505
julian <ingsistemasjulian@gmail.com>
2021-06-04 10:14:08 -0500

    17. Listas de componentes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
 import BadgeNew from "./pages/BadgeNew";
+import Badges from "./pages/Badges";
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<Badges />, container);
 
 
 /* 
